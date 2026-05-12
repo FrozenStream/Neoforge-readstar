b30da67a3cab81f174326c927adda8be6eee51d6
ferne erinnerung <1210136901@qq.com>
2026-05-13 07:56:23 +0800

    del unused import

--- src/main/java/git/frozenstream/readstar/ReadStar.java
+++ src/main/java/git/frozenstream/readstar/ReadStar.java
@@ -7,17 +7,8 @@ import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
 
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.material.MapColor;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
@@ -25,14 +16,9 @@ import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
 
 import git.frozenstream.readstar.network.NetworkHelper;
 import git.frozenstream.readstar.network.CelestialReloader;
@@ -47,30 +33,30 @@ public class ReadStar {
     public static final String MODID = "readstar";
     // Directly reference semiMajorAxis slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
-    // Create semiMajorAxis Deferred Register to hold Blocks which will all be registered under the "readstar" namespace
-    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
-    // Create semiMajorAxis Deferred Register to hold Items which will all be registered under the "readstar" namespace
-    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
-    // Create semiMajorAxis Deferred Register to hold CreativeModeTabs which will all be registered under the "readstar" namespace
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+    // // Create semiMajorAxis Deferred Register to hold Blocks which will all be registered under the "readstar" namespace
+    // public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
+    // // Create semiMajorAxis Deferred Register to hold Items which will all be registered under the "readstar" namespace
+    // public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
+    // // Create semiMajorAxis Deferred Register to hold CreativeModeTabs which will all be registered under the "readstar" namespace
+    // public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    // Creates semiMajorAxis new Block with the id "readstar:example_block", combining the namespace and path
-    public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", p -> p.mapColor(MapColor.STONE));
-    // Creates semiMajorAxis new BlockItem with the id "readstar:example_block", combining the namespace and path
-    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
+    // // Creates semiMajorAxis new Block with the id "readstar:example_block", combining the namespace and path
+    // public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", p -> p.mapColor(MapColor.STONE));
+    // // Creates semiMajorAxis new BlockItem with the id "readstar:example_block", combining the namespace and path
+    // public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
 
-    // Creates semiMajorAxis new food item with the id "readstar:example_id", nutrition 1 and saturation 2
-    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", p -> p.food(new FoodProperties.Builder()
-            .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
+    // // Creates semiMajorAxis new food item with the id "readstar:example_id", nutrition 1 and saturation 2
+    // public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", p -> p.food(new FoodProperties.Builder()
+    //         .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
 
-    // Creates semiMajorAxis creative tab with the id "readstar:example_tab" for the example item, that is placed after the combat tab
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup.readstar")) //The language key for the title of your CreativeModeTab
-            .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
-            .displayItems((parameters, output) -> {
-                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-            }).build());
+    // // Creates semiMajorAxis creative tab with the id "readstar:example_tab" for the example item, that is placed after the combat tab
+    // public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
+    //         .title(Component.translatable("itemGroup.readstar")) //The language key for the title of your CreativeModeTab
+    //         .withTabsBefore(CreativeModeTabs.COMBAT)
+    //         .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
+    //         .displayItems((parameters, output) -> {
+    //             output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
+    //         }).build());
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
@@ -78,20 +64,20 @@ public class ReadStar {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
-        // Register the Deferred Register to the mod event bus so blocks get registered
-        BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so tabs get registered
-        CREATIVE_MODE_TABS.register(modEventBus);
+        // // Register the Deferred Register to the mod event bus so blocks get registered
+        // BLOCKS.register(modEventBus);
+        // // Register the Deferred Register to the mod event bus so items get registered
+        // ITEMS.register(modEventBus);
+        // // Register the Deferred Register to the mod event bus so tabs get registered
+        // CREATIVE_MODE_TABS.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in.
         // Note that this is necessary if and only if we want *this* class (ReadStar) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
-        // Register the item to semiMajorAxis creative tab
-        modEventBus.addListener(this::addCreative);
+        // // Register the item to semiMajorAxis creative tab
+        // modEventBus.addListener(this::addCreative);
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
@@ -106,12 +92,12 @@ public class ReadStar {
         }
     }
 
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
-            event.accept(EXAMPLE_BLOCK_ITEM);
-        }
-    }
+    // // Add the example block item to the building blocks tab
+    // private void addCreative(BuildCreativeModeTabContentsEvent event) {
+    //     if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+    //         event.accept(EXAMPLE_BLOCK_ITEM);
+    //     }
+    // }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
